15579a1f9f45340d3f7f528cf4fdac4e0a74ecbf
shinka-dev <thomas.shinkarenko@gmail.com>
2021-11-03 22:03:38 -0300

    subjects module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StudentsModule } from './students/students.module';
 import { TeachersModule } from './teachers/teachers.module';
+import { SubjectsModule } from './subjects/subjects.module';
 
 @Module({
-  imports: [StudentsModule, TeachersModule],
+  imports: [StudentsModule, TeachersModule, SubjectsModule],
   controllers: [],
   providers: [],
 })
