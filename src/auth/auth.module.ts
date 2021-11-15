f75ecef18b3d62b9e09de06ca4cdbd8d44a090ca
shinka-dev <thomas.shinkarenko@gmail.com>
2021-11-15 15:59:30 -0300

    Subjects Completed + Auth WIP

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controller/auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { USersRepository } from './repository/users.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([USersRepository])],
   providers: [AuthService],
   controllers: [AuthController],
 })
